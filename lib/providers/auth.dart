be9de283eb319c6bd6614bf4ca0acf9a8f2f1d5f
Ahmed Elsayed <ahmed_elsayed7071@outlook.com>
2020-05-14 17:33:29 +0200

    Admin Login With Password Reset Through Email

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -12,8 +13,7 @@ class Auth with ChangeNotifier {
   Future<void> _authenticate(String email, String password) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxvZKA_BX1E0Ae_R8BS3L_-hyUSi1vkbo';
-
-      try {
+    try {
       final response = await http.post(
         url,
         body: json.encode(
@@ -33,7 +33,77 @@ class Auth with ChangeNotifier {
       throw error;
     }
   }
+
+  Future<void> _sendResetPasswordEmail(String email) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBxvZKA_BX1E0Ae_R8BS3L_-hyUSi1vkbo';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {"requestType": "PASSWORD_RESET", "email": email},
+        ),
+      );
+      final responseData = json.decode(response.body);
+      print(responseData);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> resetPassword(String email) async {
+    return _sendResetPasswordEmail(email);
+  }
+
   Future<void> login(String email, String password) async {
     return _authenticate(email, password);
   }
 }
+
+
+// import 'dart:convert';
+// import 'dart:io';
+
+// import 'package:flutter/widgets.dart';
+// import 'package:http/http.dart' as http;
+// // import 'package:firebase_auth/firebase_auth.dart';
+
+// class Auth with ChangeNotifier {
+//     // final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+//   String _token;
+//   DateTime _expiryDate;
+//   String _userId;
+
+//   Future<void> _authenticate(String email, String password) async {
+//     final url =
+//         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBxvZKA_BX1E0Ae_R8BS3L_-hyUSi1vkbo';
+
+//       try {
+//       final response = await http.post(
+//         url,
+//         body: json.encode(
+//           {
+//             'email': email,
+//             'password': password,
+//             'returnSecureToken': true,
+//           },
+//         ),
+//       );
+//       final responseData = json.decode(response.body);
+//       print(responseData);
+//       if (responseData['error'] != null) {
+//         throw HttpException(responseData['error']['message']);
+//       }
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+//   Future<void> login(String email, String password) async {
+//     return _authenticate(email, password);
+//   }
+
+// }
